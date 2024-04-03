cfea20fb6a7699b02d8d0750adbb28510cbe57bb
Eugene Suleimanov <proselytear@yahoo.com>
2024-04-03 08:52:07 +0300

    STEP2: Added error handling logic

---
+++ src/main/java/net/proselyte/qafordevsreactive/errorhandling/AppErrorAttributes.java
@@ -0,0 +1,70 @@
+package net.proselyte.qafordevsreactive.errorhandling;
+
+
+import net.proselyte.qafordevsreactive.exception.ApiException;
+import net.proselyte.qafordevsreactive.exception.DeveloperNotFoundException;
+import net.proselyte.qafordevsreactive.exception.DeveloperWithEmailAlreadyExistsException;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+public class AppErrorAttributes extends DefaultErrorAttributes {
+
+    public AppErrorAttributes() {
+        super();
+    }
+
+    @Override
+    public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+        var errorAttributes = super.getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        var error = getError(request);
+
+        var errorList = new ArrayList<Map<String, Object>>();
+
+        HttpStatus status;
+        if (error instanceof DeveloperWithEmailAlreadyExistsException) {
+            status = HttpStatus.BAD_REQUEST;
+            var errorMap = new LinkedHashMap<String, Object>();
+            errorMap.put("code", ((ApiException) error).getErrorCode());
+            errorMap.put("message", error.getMessage());
+            errorList.add(errorMap);
+        } else if (error instanceof DeveloperNotFoundException) {
+            status = HttpStatus.NOT_FOUND;
+            var errorMap = new LinkedHashMap<String, Object>();
+            errorMap.put("code", ((ApiException) error).getErrorCode());
+            errorMap.put("message", error.getMessage());
+            errorList.add(errorMap);
+        } else if (error instanceof ApiException) {
+            status = HttpStatus.NOT_FOUND;
+            var errorMap = new LinkedHashMap<String, Object>();
+            errorMap.put("code", ((ApiException) error).getErrorCode());
+            errorMap.put("message", error.getMessage());
+            errorList.add(errorMap);
+        } else {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            var message = error.getMessage();
+            if (message == null)
+                message = error.getClass().getName();
+
+            var errorMap = new LinkedHashMap<String, Object>();
+            errorMap.put("code", "INTERNAL_ERROR");
+            errorMap.put("message", message);
+            errorList.add(errorMap);
+        }
+
+        var errors = new HashMap<String, Object>();
+        errors.put("errors", errorList);
+        errorAttributes.put("status", status.value());
+        errorAttributes.put("errors", errors);
+
+        return errorAttributes;
+    }
+}
